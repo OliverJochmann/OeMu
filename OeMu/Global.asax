1122dca30d5897ddd6afa25d231e18fb349ebb91
Oliver Jochmann <oliver.jochmann@netzkern.de>
2019-06-04 10:36:23 +0200

    Initial Commit

---
+++ OeMu/Global.asax
@@ -0,0 +1,1 @@
+<%@ Application Codebehind="Global.asax.cs" Inherits="OeMu.MvcApplication" Language="C#" %>
---
+++ OeMu/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+using CMS;
+
+[assembly: AssemblyDiscoverable]
+
+[assembly: AssemblyTitle("OeMu")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("4b31af71-3abe-44e2-a5c7-ffe2cfb3ae49")]
+
+[assembly: AssemblyProduct("OeMu")]
+[assembly: AssemblyCopyright("© 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("12.0.0.0")]
+[assembly: AssemblyFileVersion("12.0.6900.17470")]
+[assembly: AssemblyInformationalVersion("12.0.0")]
